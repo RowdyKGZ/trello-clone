426a614ef500763052dff92438c25c6f17b58ab3
RowdyKGZ <73703199+RowdyKGZ@users.noreply.github.com>
2023-11-27 05:10:34 +0600

    start taskify

---
+++ app/(marketing)/page.tsx
@@ -0,0 +1,21 @@
+import { Medal } from "lucide-react";
+import React from "react";
+
+const MarketingPage = () => {
+  return (
+    <div className="flex items-center justify-center flex-col">
+      <div className="flex items-center justify-center flex-col">
+        <div className="mb-4 flex items-center border shadow-sm p-4 bg-amber-100 text-amber-700 rounded-full uppercase">
+          <Medal className="h-6 w-6 mr-2" />
+          No 1 task managment
+        </div>
+
+        <h1 className="text-3xl md:text-6xl text-center">
+          Taskify helps team move
+        </h1>
+      </div>
+    </div>
+  );
+};
+
+export default MarketingPage;
