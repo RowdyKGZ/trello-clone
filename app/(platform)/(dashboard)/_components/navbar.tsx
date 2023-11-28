934ffdcaf669b1fef7a3673b55ec9052074f9a6f
RowdyKGZ <73703199+RowdyKGZ@users.noreply.github.com>
2023-11-28 21:45:58 +0600

    sidebar and mobile sidebar end

--- app/(platform)/(dashboard)/_components/navbar.tsx
+++ app/(platform)/(dashboard)/_components/navbar.tsx
@@ -3,16 +3,16 @@ import { Plus } from "lucide-react";
 
 import { Logo } from "@/components/logo";
 import { Button } from "@/components/ui/button";
+import { MobileSidebar } from "./mobile-sidebar";
 
 export const Navbar = () => {
   return (
     <div className="fixed z-50 top-0 px-4 w-full h-14 border-b shadow-sm bg-white flex items-center">
-      {/* TODO: mobile sidebar */}
+      <MobileSidebar />
       <div className="flex items-center gap-x-4">
         <div className="hidden md:flex">
           <Logo />
         </div>
-
         <Button
           size="sm"
           className="rounded-sm hidden md:block h-auto py-1.5 px-2"
--- components/logo.tsx
+++ components/logo.tsx
@@ -9,7 +9,7 @@ const headingFont = localFont({ src: "../public/fonts/font.woff2" });
 export const Logo = () => {
   return (
     <Link href={"/"}>
-      <div className="hover:opacity-75 transition items-center gap-x-2 hidden md:flex">
+      <div className="hover:opacity-75 transition items-center gap-x-2 hidden md:flex px-2">
         <Image src="/logo.svg" alt="logo" width={30} height={30} />
 
         <p
