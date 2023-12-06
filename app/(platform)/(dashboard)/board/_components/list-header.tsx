86d8de0029f9e8073c224d331ff02eccce5afc87
RowdyKGZ <73703199+RowdyKGZ@users.noreply.github.com>
2023-12-06 12:54:43 +0600

    create card from list

--- app/(platform)/(dashboard)/board/_components/list-header.tsx
+++ app/(platform)/(dashboard)/board/_components/list-header.tsx
@@ -13,9 +13,10 @@ import { ListOptions } from "./list-options";
 
 interface ListHeaderProps {
   data: List;
+  onAddCard: () => void;
 }
 
-export const ListHeader = ({ data }: ListHeaderProps) => {
+export const ListHeader = ({ data, onAddCard }: ListHeaderProps) => {
   const [title, setTitle] = useState(data.title);
   const [isEditing, setIsEditing] = useState(false);
 
@@ -98,7 +99,7 @@ export const ListHeader = ({ data }: ListHeaderProps) => {
         </div>
       )}
 
-      <ListOptions data={data} onAddCard={() => {}} />
+      <ListOptions data={data} onAddCard={onAddCard} />
     </div>
   );
 };
--- app/(platform)/(dashboard)/board/_components/list-item.tsx
+++ app/(platform)/(dashboard)/board/_components/list-item.tsx
@@ -1,8 +1,13 @@
 "use client";
 
+import { ElementRef, useRef, useState } from "react";
+
 import { ListWithCard } from "@/types";
+import { cn } from "@/lib/utils";
 
 import { ListHeader } from "./list-header";
+import { CardForm } from "./card-form";
+import { CardItem } from "./card-item";
 
 interface ListItemProps {
   index: number;
@@ -10,10 +15,44 @@ interface ListItemProps {
 }
 
 export const ListItem = ({ data, index }: ListItemProps) => {
+  const textareaRef = useRef<ElementRef<"textarea">>(null);
+
+  const [isEditing, setIsEditing] = useState(false);
+
+  const disableEditing = () => {
+    setIsEditing(false);
+  };
+
+  const enableEditing = () => {
+    setIsEditing(true);
+    setTimeout(() => {
+      textareaRef.current?.focus();
+    });
+  };
+
   return (
     <li className="shrink-0 h-full w-[272px] select-none">
       <div className="w-full rounded-md bg-[#f1f2f4] shadow-md pb-2">
-        <ListHeader data={data} />
+        <ListHeader onAddCard={enableEditing} data={data} />
+
+        <ol
+          className={cn(
+            "mx-1 px-1 py-0.5 flex flex-col gap-y-2",
+            data.card.length > 0 ? "mt-2" : "mt-0"
+          )}
+        >
+          {data.card.map((card, index) => (
+            <CardItem index={index} key={card.id} data={card} />
+          ))}
+        </ol>
+
+        <CardForm
+          listId={data.id}
+          ref={textareaRef}
+          isEditing={isEditing}
+          enableEditing={enableEditing}
+          disableEditing={disableEditing}
+        />
       </div>
     </li>
   );
