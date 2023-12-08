791ae892a823acce7b1c198fce5da92a33903ebe
RowdyKGZ <73703199+RowdyKGZ@users.noreply.github.com>
2023-12-08 21:37:07 +0600

    fixed for deploy

--- app/(platform)/(dashboard)/organization/[organizationId]/activity/page.tsx
+++ app/(platform)/(dashboard)/organization/[organizationId]/activity/page.tsx
@@ -1,14 +1,17 @@
 import { Suspense } from "react";
 
 import { Separator } from "@/components/ui/separator";
+import { checkSubcription } from "@/lib/subscription";
 
 import { Info } from "../_components/info";
 import { ActivityList } from "./_components/activity-list";
 
-const ActivityPage = () => {
+const ActivityPage = async () => {
+  const isPro = await checkSubcription();
+
   return (
     <div className="w-full">
-      <Info />
+      <Info isPro={isPro} />
       <Separator />
 
       <Suspense fallback={<ActivityList.Skeleton />}>
